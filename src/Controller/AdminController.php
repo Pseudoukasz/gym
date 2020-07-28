a17785005aa8106be0efe03c49275b04f045e703
PseudoUkasz <lukas.szwalek@gmail.com>
2020-07-28 21:35:55 +0200

    nadawanie ról przez admina

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -18,10 +23,48 @@ class AdminController extends AbstractController
     {
         $users=$this->getDoctrine()->getRepository(User::class)->findAll();
         
+        $form=$this->createFormBuilder()
+        ->add('urzytkownik', EntityType::class,[
+            'class'=>User::class,
+            'choice_label'=>'email',
+        ])
+        ->add('rola', ChoiceType::class,[
+            'choices'=>[
+                'trener'=>'ROLE_TRAINER',
+                'klient'=>'ROLE_CUSTOMER',
+            ],
+        ])
+        ->add('zapisz', SubmitType::class, [
+            'attr'=>[
+                'class'=>'btn btn-danger float-rignt'
+        ]])
+        ->getForm();
+
+        $form->handleRequest($request);
+            if($form ->isSubmitted()){
+                $data = $form->getData();
+                //$data2=$data['urzytkownik'];
+                //$data3=$data['rola'];
+                $em=$this->getDoctrine()->getManager();
+                $user=$em->getRepository(User::class)->find($data['urzytkownik']);
+                $user->setRoles([$data['rola']]);
+                dump($data,$user);
+                $em->persist($user);
+                $em->flush();
+            }
+
         //dump($users);die;
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
             'users' => $users,
+            'form' => $form->createView(),
+        ]);
+    }
+    public function nadaj_uprawnienia(Request $request){
+
+        
+        return $this->render('admin/index.html.twig',[
+            
         ]);
     }
 }
