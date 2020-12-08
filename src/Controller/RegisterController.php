5c37cd101b333e70779e98ee61ee63915249f1f0
PseudoUkasz <lukas.szwalek@gmail.com>
2020-12-08 23:44:05 +0100

    register regex

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Validator\Type\FormTypeValidatorExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,6 +15,12 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\TypeValidator;
 
 class RegisterController extends AbstractController
 {
@@ -22,15 +30,51 @@ class RegisterController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder )
     {
         $form= $this->createFormBuilder()
-            ->add('email')
-            ->add('name')
-            ->add('surname')
-            ->add('phone_number')
+            ->add('email',  TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email([
+                        'message' => 'The email {{ value }} is not a valid email.',
+                    ])
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex(['pattern' => '/[a-zA-Z]+$/',
+                        'match' => true]),
+                    new Type([
+                        'type' => 'string',
+                        'message' => 'The value {{ value }} is not a valid {{ type }}.'
+                    ])
+                ],
+            ])
+            ->add('surname', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex(['pattern' => '/[a-zA-Z]+$/',
+                        'match' => true]),
+                    new Type([
+                        'type' => 'string',
+                        'message' => 'The value {{ value }} is not a valid {{ type }}.',
+                    ])
+                ],
+            ])
+            ->add('phone_number',TextType::class, [
+                'constraints' =>[
+                    new Length(['min' => 9])
+                ]
+            ]
+            )
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'required' => true,
                 'first_options' => ['label' =>'Hasło'],
-                'second_options' => ['label' =>'Powtórz hasło']
+                'second_options' => ['label' =>'Powtórz hasło'],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 8])
+                ],
             ])
             ->add('register', SubmitType::class, [
                 'attr'=>[
@@ -39,7 +83,7 @@ class RegisterController extends AbstractController
             ->getForm()
         ;
             $form->handleRequest($request);
-            if($form ->isSubmitted()){
+            if($form ->isSubmitted() && $form->isValid()){
                 $data = $form->getData();
                 $user = new User();
                 $user->setEmail($data['email']);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,6 +44,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
      */
     private $surname;
 
