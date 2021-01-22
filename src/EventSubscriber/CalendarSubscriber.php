e316a8f4f6fb614e2cde95976335b28878379855
PseudoUkasz <lukas.szwalek@gmail.com>
2021-01-22 21:02:11 +0100

    stylistyka kodu

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -17,14 +17,11 @@ use Symfony\Component\Security\Core\Security;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
-
     private $classesRepository;
     private $reservationsRepository;
     private $signForClassesRepository;
     private $router;
     private $security;
-    private $user;
-
     public function __construct(
         ClassesRepository $classesRepository,
         UrlGeneratorInterface $router,
@@ -38,8 +35,6 @@ class CalendarSubscriber implements EventSubscriberInterface
         $this->security = $security;
         $this->reservationsRepository = $reservationsRepository;
     }
-
-
     public static function getSubscribedEvents()
     {
         return [
@@ -49,17 +44,7 @@ class CalendarSubscriber implements EventSubscriberInterface
 
     public function onCalendarSetData(CalendarEvent $calendar)
     {
-        dump($calendar);
-        //$start = $calendar->getStart();
-        $start='2020-08-10 00:00:00';
-        //$dump($start);die;
-        $end='2021-08-10 00:00:00';
-        //$end = $calendar->getEnd();
         $filters = $calendar->getFilters();
-        dump($filters);
-        // Modify the query to fit to your entity and needs
-        // Change booking.beginAt by your start date property
-        //$bookings=$em->getRepository(Classes::class)->findAll();
         if ($filters['roomId'] == 'all'){
             $zajeciaa=$this->classesRepository->findAll();
         } else
@@ -72,90 +57,47 @@ class CalendarSubscriber implements EventSubscriberInterface
                 $reservation->getDateStart(),
                 $reservation->getDateEnd()
             );
-            $bookingEvent->setOptions(
-                [
+            $bookingEvent->setOptions([
                     'backgroundColor' => 'brown',
                     'borderColor' => 'brown',
-                ]
-            );
+                ]);
             $calendar->addEvent($bookingEvent);
         }
-
-        /*
-        $zajeciaa = $this->zajeciaRepository
-            ->createQueryBuilder('classes')
-            ->where('classes.data BETWEEN :start and :end')
-            ->setParameter('start', $start)
-            ->setParameter('end', $end)
-            //->setParameter('start', $start->format('Y-m-d H:i:s'))
-            //->setParameter('end', $end->format('Y-m-d H:i:s'))
-            ->getQuery()
-            ->getResult()
-        ; */
-        dump($zajeciaa);
-        
         foreach ($zajeciaa as $zajecia) {
-            // this create the events with your data (here booking data) to fill calendar
             $bookingEvent = new Event(
                 $zajecia->getName(),
                 $zajecia->getDateStart(),
                 $zajecia->getDateEnd()
-                //$classes->getIdTrener(),
-                //$classes->getSala() // If the end date is null or not defined, a all day event is created.
             );
-
-            /*
-             * Add custom options to events
-             *
-             * For more information see: https://fullcalendar.io/docs/event-object
-             * and: https://github.com/fullcalendar/fullcalendar/blob/master/src/core/options.ts
-             */
-            //var_dump($zajecia->getId());die;
-            //$zajecia->getId();
-
-            //var_dump($id);
             if($this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
-
-                if ($zajecia->getRoom()->getMaxNumberOfUsers() <= count($this->signForClassesRepository->findBy(['classes' => $zajecia->getId()]))) {
-
-                    $bookingEvent->setOptions(
-                        [
+                if ($zajecia->getRoom()->getMaxNumberOfUsers() <= count($this->signForClassesRepository->findBy(['classes' => $zajecia->getId()]))){
+                    $bookingEvent->setOptions([
                             'backgroundColor' => 'red',
                             'borderColor' => 'red',
-                        ]
-                    );
+                        ]);
                 } else if ($this->signForClassesRepository->findOneBy(['classes' => $zajecia->getId(), 'user' => $this->security->getUser()])) {
-                    $bookingEvent->setOptions(
-                        [
+                    $bookingEvent->setOptions([
                             'backgroundColor' => 'blue',
                             'borderColor' => 'blue',
-                        ]
-                    );
+                        ]);
                 }else {
-                    $bookingEvent->setOptions(
-                        [
+                    $bookingEvent->setOptions([
                             'backgroundColor' => 'green',
                             'borderColor' => 'green',
-                        ]
-                    );
+                        ]);
                 }
             } else {
-                $bookingEvent->setOptions(
-                    [
+                $bookingEvent->setOptions([
                         'backgroundColor' => 'green',
                         'borderColor' => 'green',
-                    ]
-                );
+                    ]);
             }
-
             $bookingEvent->addOption(
                 'url',
                 $this->router->generate('zajecia_show', [
                     'id' => $zajecia->getId(),
                 ])
             );
-
-            // finally, add the event to the CalendarEvent to fill the calendar
             $calendar->addEvent($bookingEvent);
         }
     }    
